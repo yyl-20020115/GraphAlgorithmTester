56a567fa6b8cfe071be38a2a90b3115d8f40eb8b
YILIN <YILIN@DESKTOP-YILIN>
2022-07-05 13:26:13 +0800

    minor changes

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ public static class Program
         var hamilton = new HamitonianCycleProblemSolver();
         var maxflows = new MaxFlowsProblemResolver();
 
-        ProblemSolver.BuildGraph("CityGraphInput.txt", traveller.Nodes, traveller.Edges);
+        ProblemSolver.BuildGraph("CityGraphInput.txt", traveller.Nodes, traveller.Edges, true);
         ProblemSolver.BuildGraph("HamiltonianCycle.txt", hamilton.Nodes, hamilton.Edges);
-        ProblemSolver.BuildGraph("MaxFlows2.txt", maxflows.Nodes, maxflows.Edges,true);
+        ProblemSolver.BuildGraph("MaxFlows1.txt", maxflows.Nodes, maxflows.Edges,true);
 
         using var writer = new StreamWriter("Result.txt");
         traveller.Solve(writer);
